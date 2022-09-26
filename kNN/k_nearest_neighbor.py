2502d0e1568696f1cffaa751d6a11f404f50da74
Elloye <maximillionixo@gmail.com>
2022-09-26 20:34:01 +0300

    Update k_nearest_neighbor.py

--- kNN/k_nearest_neighbor.py
+++ kNN/k_nearest_neighbor.py
@@ -75,7 +75,7 @@ class KNearestNeighbor:
                 # not use a loop over dimension, nor use np.linalg.norm().          #
                 #####################################################################
                 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-                dists[i, j] = np.sqrt(np.square(np.sum((X[i] - self.X_train[j]))))
+                dists[i, j] = np.sqrt(np.sum(np.square((X[i] - self.X_train[j]))))
                 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
 
@@ -97,7 +97,7 @@ class KNearestNeighbor:
             # Do not use np.linalg.norm().                                        #
             #######################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-            dists[i, :] = np.sqrt(np.square(np.sum((X[i] - self.X_train)), axis=1))
+            dists[i, :] = np.sqrt(np.sum(np.square((X[i] - self.X_train)), axis=1))
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
 
